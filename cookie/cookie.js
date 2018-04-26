b83b2d305f08cfd544c34dae0787cc9a1b2bf262
haulindaphne <haulindaphne@gmail.com>
2018-04-26 12:16:21 +0200

    cookie clicker update

--- cookie/cookie.js
+++ cookie/cookie.js
@@ -1,9 +1,8 @@
 let score = 0;
-let price = 10;
+let price = 5;
 let multiplier = 1;
-let interval = 200;
+let interval = 300;
 let display = document.getElementById("display");
-
 
 //augmentation du score à chaque click
 function increase() {
@@ -12,32 +11,78 @@ function increase() {
 }
 
 function augmenterMultiplicateur() {
+    if(multiplier <= 6) {
     multiplier = multiplier * 2;
     let result = document.getElementById('multiply');
     result.innerHTML = "Prochain multiplicateur : x " + multiplier + 
     " pour :  " + price + " points.";
     }
+    else {
+            price += multiplier + (price * 2) || score > 0;
+        }
+    }
+
 
 function buyMultiplicator() {
     if(score >= price) {
         augmenterMultiplicateur()
         score = score - price;
         price = price *2;
-        document.getElementById('diplay').value = score;
+        document.getElementById('display').value = score;
+    }
+    else {
+        alert("Vous n'avez pas assez de points! Cliquez encore.");
     }
 }
 //Autoclick
 function clicker() {
-    if(score >= interval) {
-        setInterval(increase, 1000);
-}
+    if(score >= 200) {
+        let time = setInterval(increase, 1000); 
+        score = score - 200;
+        document.querySelector('#autoCursor').style.visibility = "hidden";
+        }
  else {
         alert('Vous êtes fauché! \nContinuez à cliquer!!!');
     }
 }
 
-//achat du bonus
+function autoClicker() {
+    if(score >= 500) {
+        setInterval(increase, 500);
+        score = score - 500;
+        document.querySelector('#autoclic').style.visibility = "hidden";
+    }
+    else {
+        alert('Encore fauché? Clique plus vite!');
+    }
+}
 
+//achat du bonus
+function bonus() {
+    if(score >= 5000) {
+        setInterval(increaseBonus, 250);
+        score = score - 5000;
+        document.querySelector('#bonusBtn').style.visibility = 'hidden';
+        }
+        else {
+        alert("Vous êtes encore fauché? Mais cliquez plus vite!!!")
+    }
+}
+function doubleBonus(){
+    bonus();
+    newCount();
+    while (counter === 0) {
+    clearInterval(newCount);
+    clearInterval(increaseBonus);
+    document.querySelector('#bonusBtn').style.visibility = 'visible';
+
+    }
+}
+
+function increaseBonus(){
+    score += (2 * multiplier);
+    document.getElementById('display').innerHTML = score;
+}
 
 //fonction save
 function save() {
@@ -50,3 +95,15 @@ function load() {
     document.getElementById('display').innerHTML = score;
 }
 
+//compteur
+let counter = 30;
+function newCount() {
+let newCount = setInterval(function() {
+    console.log(counter);
+    counter--;
+    if(counter === 0) {
+        alert("C'est fini!");
+        clearInterval(newCount);
+    }
+}, 1000);
+}
---
+++ lesPaires/lesPaires.js
@@ -0,0 +1,5 @@
+//1. Construction du tableau de cartes
+const cardsArray = [{
+    'name': 'Ane',
+    'img': 'asset/anehihan.jpg',
+}];
